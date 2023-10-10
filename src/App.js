2fc225720e49bc146ff4c8a773473ba699eb3b81
Code Engineers <86651303+ecodeengineers@users.noreply.github.com>
2023-10-10 17:27:46 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import "./css/stylesheet.css";
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./component/Home";
 import { Product } from "./Product";
+import {Login} from "./login"
+import {Dashboard} from "./dashboard"
 import { NoMatch } from "./component/NoMatch";
+import { CustomerList } from "./customerlist";
+import { CustomerActions } from "./component/Dashboard/Customer/CustomerActions";
 
 function App() {
 
@@ -14,6 +18,10 @@ function App() {
       <Routes>
         <Route index  path="/" element={<Home />} />
         <Route path="/Product" element={<Product />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/customerlist" element={<CustomerList />} />
+        <Route path="/customerActions" element={<CustomerActions />} />
         <Route path="*" element={<NoMatch />} />
       </Routes>
     </>
